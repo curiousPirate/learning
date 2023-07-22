ec40393a6f3dac5de0c2ff83c0b1be92e2fe32fd
Chaitanya Varma <gchaitanya.369@gmail.com>
2023-07-22 23:30:22 +0530

    second version of black jack

--- src/BlackJack.js
+++ src/BlackJack.js
@@ -1,53 +1,53 @@
-// alert("Get ready to play Black Jack!");
+alert("Get ready to play Black Jack!");
 
-// // Initial scores
-// let player = 0;
-// let dealer = 0;
+// Initial scores
+let player = 0;
+let dealer = 0;
 
-// // Probability of card nos. for dealer and player
-// const gameSet = [4, 5, 6, 7, 8]
+// Probability of card nos. for dealer and player
+const gameSet = [4, 5, 6, 7, 8]
 
-// // Random set generator
-// let random = Math.floor(Math.random(gameSet) * gameSet.length);
-// random = gameSet[random];
+// Random set generator
+let random = Math.floor(Math.random(gameSet) * gameSet.length);
+random = gameSet[random];
 
-// // Player turn
-// alert("Player's turn")
+// Player turn
+alert("Player's turn")
 
-// player += random;
+player += random;
 
-// let playerChoice = confirm("You got " + random + ". Now your total is " + player + ", Do you want to continue?")
-// while ( playerChoice === true && player < 22 ) {
-//   playerChoice = confirm("You got " + random + ". Now your total is " + player + ", Do you want to continue?");
-//   player += random;
-// }
+let playerChoice = confirm("You got " + random + ". Now your total is " + player + ", Do you want to continue?")
+while ( playerChoice === true && player < 22 ) {
+  playerChoice = confirm("You got " + random + ". Now your total is " + player + ", Do you want to continue?");
+  player += random;
+}
 
-// // Dealer chance
-// alert("Now Dealers turn")
-// while ( dealer <= player && dealer < 22) {
-//       dealer += random;
-//       alert("Dealer's hand: " + dealer);
-// }
+// Dealer chance
+alert("Now Dealers turn")
+while ( dealer <= player && dealer < 22) {
+      dealer += random;
+      alert("Dealer's hand: " + dealer);
+}
 
-// // Player game status
-// if (player > dealer && player < 22) {
-//   alert("Player won!");
-// } else if (dealer === 21) {
-//   alert("Player BLACKJACK!");
-// } else if (player === dealer) {
-//   alert("It's a tie!");
-// } else {
-//     alert("Player Lost!");
-// }
+// Player game status
+if (player > dealer && player < 22) {
+  alert("Player won!");
+} else if (dealer === 21) {
+  alert("Player BLACKJACK!");
+} else if (player === dealer) {
+  alert("It's a tie!");
+} else {
+    alert("Player Lost!");
+}
 
-// // Dealer game status
-// if (dealer > player && dealer < 22) {
-//   alert("Dealer won!")
-// } else if (dealer === 21) {
-//   alert("Dealer BLACKJACK!")
-// } else {
-//   alert("Dealer Lost!")
-// }
+// Dealer game status
+if (dealer > player && dealer < 22) {
+  alert("Dealer won!")
+} else if (dealer === 21) {
+  alert("Dealer BLACKJACK!")
+} else {
+  alert("Dealer Lost!")
+}
 
 // ChatGPT version
 function getRandomCard() {
